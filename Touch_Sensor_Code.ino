3e22117ccf014e02d1bb4cbeb1215a15e805876c
csoltero109 <chrissoltero109@gmail.com>
2018-12-29 00:26:48 -0800

    First Commit

---
+++ Touch_Sensor_Code.ino
@@ -0,0 +1,15 @@
+#define touchPin 2
+int count = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(touchPin, INPUT);
+}
+
+void loop() {
+  int touchValue = digitalRead(touchPin);
+  if(touchValue == HIGH){
+    count++;
+    Serial.println("Touched " + String(count) + " times");
+    }
+    delay(300);
+}
